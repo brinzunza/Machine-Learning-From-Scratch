4d2a731a16d5e9b929568a428bd88ec7ce5b453d
Bruno Inzunza <82187489+brinzunza@users.noreply.github.com>
2025-02-04 12:33:00 -0600

    Update logisticRegression.py

--- LogisticRegression/logisticRegression.py
+++ LogisticRegression/logisticRegression.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-class LinearRegression: 
+class LogisticRegression: 
 
     def __init__(self, learning_rate=0.01, iterations=1000):
         self.learning_rate = learning_rate
