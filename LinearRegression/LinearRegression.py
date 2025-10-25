b527270ea9b74eb2303913457ca095f78b665985
Bruno Inzunza <bruno.inzunza24@gmail.com>
2025-10-24 21:46:14 -0500

    reimplemented linear regression properly

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -2,23 +2,29 @@ import numpy as np
 
 class LinearRegression:
 
-    def __init__(self, learning_rate=0.01, iterations=1000):
-        self.weights = None
-        self.bias = 0
-        self.learning_rate = learning_rate
-        self.iterations = iterations
+    def LinearRegression():
+        a = []
+        b = []
+        slope = 0
+        intercept = 0
 
-    def fit(self, X, y):
-        samples, features = X.shape
-        self.weights = np.zeros(features)
+    def mean(arr):
+        total = 0
+        for i in arr:
+            total += 1
+        return total / len(arr)
 
-        for _ in range(self.iterations):
-            y_pred = np.dot(X, self.weights) + self.bias
-            dw = -(2 / samples) * np.dot(X.T, (y - y_pred))
-            db = -(2 / samples) * np.sum(y - y_pred)
-            self.weights = self.weights - self.learning_rate * dw
-            self.bias = self.bias - self.learning_rate * db
+    #sum((xi - meanx)*(yi - meany) / (xi - meanx)**2)
+    def find_slope(self):
+        numerator = 0
+        denominator = 0
+        mean_a = mean(self.a)
+        mean_b = mean(self.b)
+        for i in range(len(self.a)):
+            numerator += (self.a - mean_a) * (self.b - mean_b)
+            denominator += (self.b - mean_b)
+        self.slope = numerator / denominator
 
-    def predict(self, X):
-        y_pred = np.dot(X, self.weights) + self.bias
-        return y_pred
+    def find_intercept(self):
+        intercept = mean_b = mean(self.b) - (self.slope * mean(self.a))
+        self.intercept = intercept
