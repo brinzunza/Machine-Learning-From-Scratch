2920179d0333d3d98740083041899f76c51f9adf
Bruno Inzunza <bruno.inzunza24@gmail.com>
2025-10-24 22:20:03 -0500

    fixed linear regression implementation and added testers

--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -1,30 +1,28 @@
-import numpy as np
-
 class LinearRegression:
 
-    def LinearRegression():
-        a = []
-        b = []
-        slope = 0
-        intercept = 0
+    def __init__(self, a):
+        self.a = [point[0] for point in a]
+        self.b = [point[1] for point in a]
+        self.slope = 0
+        self.intercept = 0
 
-    def mean(arr):
+    def mean(self, arr):
         total = 0
         for i in arr:
-            total += 1
+            total += i
         return total / len(arr)
 
     #sum((xi - meanx)*(yi - meany) / (xi - meanx)**2)
     def find_slope(self):
         numerator = 0
         denominator = 0
-        mean_a = mean(self.a)
-        mean_b = mean(self.b)
+        mean_a = self.mean(self.a)
+        mean_b = self.mean(self.b)
         for i in range(len(self.a)):
-            numerator += (self.a - mean_a) * (self.b - mean_b)
-            denominator += (self.b - mean_b)
+            numerator += (self.a[i] - mean_a) * (self.b[i] - mean_b)
+            denominator += (self.a[i] - mean_a) ** 2
         self.slope = numerator / denominator
 
     def find_intercept(self):
-        intercept = mean_b = mean(self.b) - (self.slope * mean(self.a))
+        intercept = mean_b = self.mean(self.b) - (self.slope * self.mean(self.a))
         self.intercept = intercept
